027b06bce1d0aaef607da3561167f2cbf9b08e67
JeanPaulMath <47893963+JeanPaulMath@users.noreply.github.com>
2019-09-24 11:45:24 -0400

    Added tab views, forms, and button to project
    Simple button function was added.

--- Controller.java
+++ Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+// fxml ids for SceneBuilder
+    @FXML
+    private Button SubmitBtn;
+
+// sample method for pressing the submit button
+    public void submitClicked(){
+        SubmitBtn.setText("ouch");
+    }
+
 }
