5489d14570b4873686cc561cdfe822bd73c9f51a
JeanPaulMath <47893963+JeanPaulMath@users.noreply.github.com>
2019-09-24 22:28:07 -0400

    Connected to H2
    Using a test table for the select statement

--- Main.java
+++ Main.java
@@ -9,16 +9,36 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-
 import java.io.IOException;
+import java.sql.*;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        //connecting to H2 database
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test","sa","");
+
+        //sample select Statement from test table that is provided by H2
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery("SELECT  * FROM  TEST");
+
+        While(resultSet.next()); {
+            System.out.println(resultSet.getString("NAME"));
+        }
+
+        conn.close();
+
+
         launch(args);
     }
 
-//primary stage method that displays the sample fxml file
+    private static void While(boolean next) {
+
+    }
+
+    //primary stage method that displays the sample fxml file
     @Override
     public void start(Stage primaryStage) throws IOException {
 
@@ -29,7 +49,6 @@ public class Main extends Application {
 
 
     }
-
 
 
 }
